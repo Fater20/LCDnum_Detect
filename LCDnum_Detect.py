a4ee88d2c38025512c0d00e7558b7ce227ef9f16
Fater20 <yanyuheng20@gmail.com>
2021-08-03 16:11:04 +0800

    Update

--- LCDnum_Detect.py
+++ LCDnum_Detect.py
@@ -119,108 +119,140 @@ try:
 
         color_image = np.asanyarray(color_frame.get_data())
         imgResult = color_image.copy()
-
+        imgContours = color_image.copy()
+        image1 = color_image.copy()
+        image2 = color_image.copy()
         # 将输入转换为灰度图片
         gray = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
         # 进行高斯模糊操作
         blurred = cv2.GaussianBlur(gray, (5, 5), 0)
         # 执行边缘检测
         edged = cv2.Canny(blurred, 50, 200, 255)
-        cv2.imwrite('edge.png', edged)
 
-        # 在边缘检测map中发现轮廓
+        # 寻找轮廓
         cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
         # 根据大小对这些轮廓进行排序
         cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
         displayCnt = None
 
+        approxContours = []
         # 循环遍历所有的轮廓
         for c in cnts:
             # 对轮廓进行近似
             peri = cv2.arcLength(c, True)
             approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-
+            approxContours.append(approx)
             # 如果当前的轮廓有4个顶点，我们返回这个结果，即LCD所在的位置
-            if len(approx) == 4:
-                displayCnt = approx
-                break
+            # if len(approx) == 4:
+            #     displayCnt = approx
+            #     break
+        
+        rect = cv2.minAreaRect(max(approxContours, key=cv2.contourArea))
+        box = np.int0(cv2.boxPoints(rect))
+        cv2.polylines(imgContours, [box], True, (255, 0, 0), 2)
 
-        # 应用视角变换到LCD屏幕上
-        warped = four_point_transform(gray, displayCnt.reshape(4, 2))
-        cv2.imwrite('warped.png', warped)
-        imgResult = four_point_transform(color_image, displayCnt.reshape(4, 2))
-
-        # 使用阈值进行二值化
-        thresh = cv2.threshold(warped, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-        cv2.imwrite('thresh1.png', thresh)
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
-        # 使用形态学操作进行处理
-        thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-        cv2.imwrite('thresh2.png', thresh)
-
-        # 在阈值图像中查找轮廓，然后初始化数字轮廓列表
-        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
         digitCnts = []
+        if box is not None:
+            # 应用视角变换到LCD屏幕上
+            warped = four_point_transform(gray, box.reshape(4, 2))
 
-        # 循环遍历所有的候选区域
-        for c in cnts:
-            # 计算轮廓的边界框
-            (x, y, w, h) = cv2.boundingRect(c)
+            imgResult = four_point_transform(color_image, box.reshape(4, 2))
 
-            # 如果当前的这个轮廓区域足够大，它一定是一个数字区域
-            if w >= 15 and (h >= 30 and h <= 40):
-                digitCnts.append(c)
+            # 使用阈值进行二值化
+            thresh = cv2.threshold(warped, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+            image1 = thresh
+            # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
+            # # 使用形态学操作进行处理
+            # thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+            # image2 = thresh
 
-        # 从左到右对这些轮廓进行排序
-        digitCnts = contours.sort_contours(digitCnts, method="left-to-right")[0]
-        digits = []
+            # Set kernel as 3*3
+            kernel = np.ones((3,3),np.uint8)
+            # Erode image
+            erode_thresh = cv2.erode(thresh, kernel, iterations=2)
+            # Dilate image
+            opening_thresh = cv2.dilate(erode_thresh, kernel, iterations=4)
+            image2 = opening_thresh
+            # 在阈值图像中查找轮廓，然后初始化数字轮廓列表
+            cnts = cv2.findContours(opening_thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cnts = imutils.grab_contours(cnts)
+            
+            # 循环遍历所有的候选区域
+            for c in cnts:
+                # 计算轮廓的边界框
+                (x, y, w, h) = cv2.boundingRect(c)
+                print(w,h)
+                #print(w,h)
+                # 如果当前的这个轮廓区域足够大，它一定是一个数字区域
+                # if w >= 15 and (h >= 30 and h <= 40):
+                #     digitCnts.append(c)
+                if w >= 8 and (h >= 10 and h <= 40):
+                    digitCnts.append(c)
+            
+            cv2.polylines(imgResult, digitCnts, True, (255, 0, 0), 2)
 
-        # 循环处理每一个数字
-        i = 0
-        for c in digitCnts:
-            # 获取ROI区域
-            (x, y, w, h) = cv2.boundingRect(c)
-            roi = thresh[y:y + h, x:x + w]
+        if digitCnts:
+            # 从左到右对这些轮廓进行排序
+            digitCnts = contours.sort_contours(digitCnts, method="left-to-right")[0]
+            digits = []
 
-            # 分别计算每一段的宽度和高度
-            (roiH, roiW) = roi.shape
-            (dW, dH) = (int(roiW * 0.25), int(roiH * 0.15))
-            dHC = int(roiH * 0.05)
+            # boundingBoxes = [cv2.boundingRect(c) for c in digitCnts]
+            # print(digitCnts)
+            # print(boundingBoxes)
+            # (cnts, boundingBoxes) = zip(*sorted(zip(digitCnts, boundingBoxes),
+            #                                 key=lambda b: b[1][0], reverse=False))
+            # digitCnts = cnts
+            # digits = []
 
-            # 定义一个7段数码管的集合
-            segments = [
-                ((0, 0), (w, dH)),	                         # 上
-                ((0, 0), (dW, h // 2)),                      # 左上
-                ((w - dW, 0), (w, h // 2)),	                 # 右上
-                ((0, (h // 2) - dHC) , (w, (h // 2) + dHC)), # 中间
-                ((0, h // 2), (dW, h)),	                     # 左下
-                ((w - dW, h // 2), (w, h)),	                 # 右下
-                ((0, h - dH), (w, h))	                     # 下
-            ]
-            on = [0] * len(segments)
+            # 循环处理每一个数字
+            i = 0
+            for c in digitCnts:
+                # 获取ROI区域
+                (x, y, w, h) = cv2.boundingRect(c)
+                roi = thresh[y:y + h, x:x + w]
 
-            # 循环遍历数码管中的每一段
-            for (i, ((xA, yA), (xB, yB))) in enumerate(segments):  # 检测分割后的ROI区域，并统计分割图中的阈值像素点
-                segROI = roi[yA:yB, xA:xB]
-                total = cv2.countNonZero(segROI)
-                area = (xB - xA) * (yB - yA)
+                # 分别计算每一段的宽度和高度
+                (roiH, roiW) = roi.shape
+                (dW, dH) = (int(roiW * 0.25), int(roiH * 0.15))
+                dHC = int(roiH * 0.05)
 
-                # 如果非零区域的个数大于整个区域的一半，则认为该段是亮的
-                if total / float(area) > 0.5:
-                    on[i]= 1
+                # 定义一个7段数码管的集合
+                segments = [
+                    ((0, 0), (w, dH)),	                         # 上
+                    ((0, 0), (dW, h // 2)),                      # 左上
+                    ((w - dW, 0), (w, h // 2)),	                 # 右上
+                    ((0, (h // 2) - dHC) , (w, (h // 2) + dHC)), # 中间
+                    ((0, h // 2), (dW, h)),	                     # 左下
+                    ((w - dW, h // 2), (w, h)),	                 # 右下
+                    ((0, h - dH), (w, h))	                     # 下
+                ]
+                on = [0] * len(segments)
 
-            # 进行数字查询并显示结果
-            digit = DIGITS_LOOKUP[tuple(on)]
-            digits.append(digit)
-            cv2.rectangle(imgResult, (x, y), (x + w, y + h), (0, 255, 0), 1)
-            cv2.putText(imgResult, str(digit), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
+                # 循环遍历数码管中的每一段
+                for (i, ((xA, yA), (xB, yB))) in enumerate(segments):  # 检测分割后的ROI区域，并统计分割图中的阈值像素点
+                    segROI = roi[yA:yB, xA:xB]
+                    total = cv2.countNonZero(segROI)
+                    area = (xB - xA) * (yB - yA)
 
-        # 显示最终的输出结果
-        #print(u"{}{}.{} \u00b0C".format(*digits))
+                    # 如果非零区域的个数大于整个区域的一半，则认为该段是亮的
+                    if total> (0.5 * float(area)) :
+                        on[i]= 1
 
-        imgStack = stackImages(0.6,([color_image, imgResult]))
+                # 进行数字查询并显示结果
+                try:
+                    digit = DIGITS_LOOKUP[tuple(on)]
+                    digits.append(digit)
+                    cv2.rectangle(imgResult, (x, y), (x + w, y + h), (0, 255, 0), 1)
+                    cv2.putText(imgResult, str(digit), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
+                    # 显示最终的输出结果
+                except KeyError:
+                    print("no detect")
+            print(digits)
+            print("######")
+
+
+        imgStack = stackImages(0.5,([color_image, imgResult],[imgContours,edged],[image1,image2]))
 
         cv2.namedWindow('Detect', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('Detect', imgStack)
